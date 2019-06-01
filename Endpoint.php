06c431ba690fbe70ea89a406459e85ae194c561e
t3kj3wlz <outlawstar4761@gmail.com>
2019-06-01 00:10:23 -0500

    securing endpoint

--- Endpoint.php
+++ Endpoint.php
@@ -5,11 +5,55 @@ require_once '/var/www/html/libs/LOE/factory.php';
 
 class EndPoint extends API{
 
+    const ACCOUNTS = 'http://api.outlawdesigns.io:9661/';
+    const GETERR = 'Can only GET this endpoint';
+    const POSTERR = 'Can only POST this endpoint';
+    const REQERR = 'Malformed Request.';
+    protected static $_authErrors = array(
+      "headers"=>"Missing required headers.",
+      "noToken"=>"Access Denied. No Token Present.",
+      "badToken"=>"Access Denied. Invalid Token"
+    );
+
     public function __construct($request,$origin)
     {
         parent::__construct($request);
+        if(isset($this->headers['request_token']) && ! isset($this->headers['password'])){
+          throw new \Exception(self::$_authErrors['headers']);
+        }elseif(!isset($this->headers['auth_token']) && !isset($this->headers['request_token'])){
+          throw new \Exception(self::$_authErrors['noToken']);
+        }elseif(!$this->_verifyToken() && !isset($this->headers['request_token'])){
+          throw new \Exception(self::$_authErrors['badToken']);
+        }
     }
-
+    private function _verifyToken(){
+      $ch = curl_init();
+      curl_setopt($ch,CURLOPT_URL,self::ACCOUNTS . "verify/");
+      curl_setopt($ch,CURLOPT_HTTPHEADER,array('auth_token: ' . $this->headers['auth_token']));
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+      $output = json_decode(curl_exec($ch));
+      curl_close($ch);
+      if(isset($output->error)){
+        return false;
+      }
+      $this->user = $output;
+      return true;
+    }
+    private function _authenticate(){
+      $headers = array('request_token: ' . $this->headers['request_token'],'password: ' . $this->headers['password']);
+      $ch = curl_init();
+      curl_setopt($ch,CURLOPT_URL,self::ACCOUNTS . "authenticate/");
+      curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+      $output = json_decode(curl_exec($ch));
+      curl_close($ch);
+      if(isset($output->error)){
+        throw new \Exception($output->error);
+      }
+      $this->headers['auth_token'] = $output->token;
+      $this->_verifyToken();
+      return $output;
+    }
     protected function example(){
         return array("endPoint"=>$this->endpoint,"verb"=>$this->verb,"args"=>$this->args,"request"=>$this->request);
     }
